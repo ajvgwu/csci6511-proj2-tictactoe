45c697cff743bd84237ebf30dde854655262edee
Alex Velazquez <alexander.velazquez@nrl.navy.mil>
2018-03-01 14:07:47 -0500

    Removed constant NAME

--- src/main/java/edu/gwu/ai/codeknights/tictactoe/filtering/chooser/AnyCellChooser.java
+++ src/main/java/edu/gwu/ai/codeknights/tictactoe/filtering/chooser/AnyCellChooser.java
@@ -7,8 +7,6 @@ import edu.gwu.ai.codeknights.tictactoe.filtering.core.TicTacToeGame;
 
 public class AnyCellChooser extends AbstractCellChooser {
 
-  public static final String NAME = "AnyCellChooser";
-
   @Override
   public Cell chooseCell(final Stream<Cell> input, final TicTacToeGame game) {
     return input.findAny().orElse(null);
