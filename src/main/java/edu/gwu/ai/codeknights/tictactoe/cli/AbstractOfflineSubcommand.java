85c4bcc7cb6ece2710c520f7922ac297ac73a9f3
Alex Velazquez <36482251+ajvgwu@users.noreply.github.com>
2018-03-06 14:41:54 -0500

    Fixed parsing of board state option

--- src/main/java/edu/gwu/ai/codeknights/tictactoe/cli/AbstractOfflineSubcommand.java
+++ src/main/java/edu/gwu/ai/codeknights/tictactoe/cli/AbstractOfflineSubcommand.java
@@ -6,8 +6,8 @@ import picocli.CommandLine.Option;
 public abstract class AbstractOfflineSubcommand extends AbstractSubcommand {
 
   @Option(
-    names = {"-s", "--state"},
-    description = "initial board state")
+    names = {"-s", "--state"}, arity = "0..*",
+    description = "initial board state; player1 moves given by \"X\"; player2 moves given by \"O\"; empty cells given by any other string")
   private String[] stateArgs = null;
 
   @Override
